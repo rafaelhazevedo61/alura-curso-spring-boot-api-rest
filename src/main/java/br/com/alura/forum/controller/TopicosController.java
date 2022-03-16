ad38d63f7789a9c14821be6f05879a3b1931d33e
unknown <rafaelhazevedo61@gmail.com>
2022-03-15 23:46:45 -0300

    aula 3 - parcial - consulta com filtros

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -17,14 +17,18 @@ public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
-		
-		List<Topico> topicos = topicoRepository.findAll();
-		
-		return TopicoDto.converter(topicos);
-		
+	public List<TopicoDto> lista(String nomeCurso) {
+
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+
 	}
-	
+
 }
