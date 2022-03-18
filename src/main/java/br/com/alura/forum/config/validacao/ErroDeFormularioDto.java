73e188e62673e3f7630943136003475f90a0e202
unknown <rafaelhazevedo61@gmail.com>
2022-03-17 23:54:43 -0300

    commit aula 6 - parcial - detalhando topicos

--- src/main/java/br/com/alura/forum/config/validacao/ErroDeFormularioDto.java
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeFormularioDto.java
@@ -1,30 +1,23 @@
 package br.com.alura.forum.config.validacao;
 
 public class ErroDeFormularioDto {
-
+	
 	private String campo;
-	private String mensagem;
-
-	public ErroDeFormularioDto(String campo, String mensagem) {
-		super();
+	private String erro;
+	
+	public ErroDeFormularioDto(String campo, String erro) {
 		this.campo = campo;
-		this.mensagem = mensagem;
+		this.erro = erro;
 	}
 
 	public String getCampo() {
 		return campo;
 	}
 
-	public void setCampo(String campo) {
-		this.campo = campo;
+	public String getErro() {
+		return erro;
 	}
-
-	public String getMensagem() {
-		return mensagem;
-	}
-
-	public void setMensagem(String mensagem) {
-		this.mensagem = mensagem;
-	}
+	
+	
 
 }
