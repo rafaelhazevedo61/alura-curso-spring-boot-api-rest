9e191d7d617b7bae8d687a0e1b4ba124bff0d165
unknown <rafaelhazevedo61@gmail.com>
2022-03-18 21:00:29 -0300

    commit aula 6 - parcial - removendo topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,13 +28,13 @@ import br.com.alura.forum.repository.TopicoRepository;
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
-	
+
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@Autowired
 	private CursoRepository cursoRepository;
-	
+
 	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso) {
 		if (nomeCurso == null) {
@@ -44,40 +45,42 @@ public class TopicosController {
 			return TopicoDto.converter(topicos);
 		}
 	}
-	
+
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
-		
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
-	
+
 	@GetMapping("/{id}")
 	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
-		
+
 		Topico topico = topicoRepository.getById(id);
 		return new DetalhesDoTopicoDto(topico);
-		
+
 	}
-	
+
 	@PutMapping("/{id}")
 	@Transactional
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
-		
+
 		Topico topico = form.atualizar(id, topicoRepository);
-		
+
 		return ResponseEntity.ok(new TopicoDto(topico));
-		
+
 	}
-	
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+
+	}
 
 }
-
-
-
-
-
-
-
